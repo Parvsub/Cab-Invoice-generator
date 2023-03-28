1779bef5cc09448a7c3343e9054f89450852e377
Parvsub <Praveenboda421@gmail.com>
2023-03-28 10:59:23 +0530

    Added: Welcome message is displayed

---
+++ welcomemessage.java
@@ -0,0 +1,7 @@
+package com.cabinvoicegenerator;
+
+public class welcomemessage {
+    public static void main(String[] args) {
+        System.out.println("Weicome to cab invoice genarator");
+    }
+}
